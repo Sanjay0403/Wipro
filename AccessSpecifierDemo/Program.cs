0ea72e64d4252e14e326b465173ba4a5fa42c6af
Sanjay0403 <Sanjaydeep098@gmail.com>
2025-01-29 11:34:03 +0530

    Access Specifiers

---
+++ AccessSpecifierDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using AccessModifiers;
+
+namespace AccessSpecifierDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Customer cst = new Customer();
+            cst.CustId = 123;
+            cst.CustName = "Sanjaydeep";
+            cst.Display();
+
+        
+        
+        }
+    }
+}
