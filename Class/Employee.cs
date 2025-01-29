9a12affa4801a4383504c6f9b49b8e55039fcd6e
Sanjay0403 <Sanjaydeep098@gmail.com>
2025-01-29 11:26:45 +0530

    Class

---
+++ Class/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class Employee
+    {
+        // Members(Attributes) & Methods(Behaviour/functions)
+        public int emp_Id { get; set; }
+        public string name { get; set; }
+
+        //Constructor - Parameter
+        public Employee(int eid, string ename)
+        {
+            Console.WriteLine("Default Constructor");
+            emp_Id = eid;   
+            name = ename;
+        }
+        public Employee()
+        {
+            Console.WriteLine("Default Constructor");
+
+        }
+
+    }
+}
---
+++ Class/Program.cs
@@ -0,0 +1,31 @@
+﻿using Class;
+namespace Class
+{
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Employee emp = new Employee();
+            {
+                emp.emp_Id = 1;
+                emp.name = "Sanjay";
+
+                Console.WriteLine($"Employee ID: {emp.emp_Id}");
+                Console.WriteLine($"Employee Name: {emp.name}");
+                //reading the inputs
+                Console.WriteLine("Enter the Employee Id:");
+                int id = Convert.ToInt32( Console.ReadLine() );
+
+                Console.WriteLine("Enter the Employee Name:");
+                string? name = Console.ReadLine();
+                //creating instance
+                Employee emp2 = new Employee(id, name); //CONSTRUCTOR WILL BE CALLED INSTANCE CREATION
+                //displaying instance
+                Console.WriteLine($"Emplyee ID: {emp2.emp_Id}");
+                Console.WriteLine($"Employee Name: {emp2.name}");
+            }
+        }
+    }
+}
