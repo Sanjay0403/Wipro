3d8f1cfdf819947bc91d182bc5e013d03264166f
Sanjay0403 <Sanjaydeep098@gmail.com>
2025-01-29 11:31:39 +0530

    Day Two

---
+++ WiproDayTwo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+namespace StringAppend
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //immutable 
+            string? str = "Hello"; //memory will be allocataed
+            //for(int i = 0; i < 5; i++)
+            //{
+            str += "World"; //new string in memory will be created
+            // New memory
+            //str = "sanjay";
+            //}
+            Console.WriteLine(str);
+
+
+            //STRING BUILDER - MUTABLE 
+            StringBuilder sb = new StringBuilder("Hello");//memory will appended and stored in the same memory
+            sb.Append("World. This is Sanjaydeep");
+            Console.WriteLine(sb);
+
+            //
+            Console.WriteLine("Enter the string");
+            string? userinput = Console.ReadLine();
+            Console.WriteLine("Enter the string to get appended:");
+            string? appendedstr = Console.ReadLine();
+            userinput += appendedstr; //a+=b = a+b
+            Console.WriteLine(userinput);
+            
+            Console.WriteLine("Enter string builder input: ");
+            string? strsb = Console.ReadLine();
+
+            StringBuilder sbobj = new StringBuilder(strsb);
+
+            // StringBuilder sbobj = new StringBuilder(Console.ReadLine());
+            sbobj.Append(appendedstr);
+            Console.WriteLine(sbobj);
+        }
+
+    }
+
+}
