870ab1689f353ad08ebe9eb1fbe9b93d82328412
sanjay <118151384+Sanjay0403@users.noreply.github.com>
2025-01-30 15:18:10 +0530

    Get Set Methods

---
+++ Oops Concepts/GetSetmethods/Program.cs
@@ -0,0 +1,26 @@
+﻿using GetSetmethods;
+using System.Security.Cryptography.X509Certificates;
+
+namespace GetSetmethod
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            Student stu = new Student();
+            stu.Stuid = 11;
+            stu.Name = "Sanjay";
+            stu.Age = 4;
+
+            Console.WriteLine(stu.Stuid);
+            Console.WriteLine(stu.Name);
+            Console.WriteLine(stu.Age);
+
+
+            //stu.SId = 111;
+            //Console.WriteLine(stu.SId);
+
+        }
+    }
+}
---
+++ Oops Concepts/GetSetmethods/Student.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSetmethods
+{
+    internal class Student
+    {
+        private int sid;
+
+        public int Stuid
+        {
+            get
+            {
+                return sid;
+            }
+            set
+            {
+                sid = value;
+            }
+        }
+        private string sname;
+
+        public string Name
+        {
+            get
+            {
+                return sname.ToUpper();
+            }
+            set
+            {
+                if (value=="sanjay")
+                {
+                    Console.WriteLine("sanjay is not allowed");
+                }
+                else
+                {
+                    sname = value;
+                }
+            }
+        }
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                if (value > 5 && value>20)
+                {
+                    age = value;
+                }
+                else
+                {
+                    Console.WriteLine("Age not allowed");
+                }
+                
+            }
+        }
+    }
+}
