9d5d0d0c7736abc69029ccc0ea27f5146d4351a7
sanjay <118151384+Sanjay0403@users.noreply.github.com>
2025-01-30 15:23:28 +0530

    Abstract class method

---
+++ Oops Concepts/AbstractMethods/Flight.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractMethods
+{
+    abstract class Flight // if you want to add abstract then the class should also be in the form of an abstract class
+    {
+
+        public int FlightNo{  get; set; }
+        public string FlightName {  get; set; }
+        public void FlightDetails() //method definition -normal method { }
+        {
+            Console.WriteLine(FlightNo+" "+ FlightName);
+        }
+
+        public abstract void FareDetails(); //Method declaration - abstract keyword
+    }
+    class FlightFare : Flight
+    {
+        public decimal BasicFare {  get; private set; }
+        public int durationinhrs { get; set; }
+        public string typedes {  get; set; }
+
+        public override void FareDetails()
+        {
+            if (durationinhrs > 4)
+            {
+                BasicFare = 8000;
+            }
+            else
+            {
+                BasicFare = 5000;
+
+            }
+            if (typedes.ToLower().Equals("International"))
+            {
+                BasicFare += 10000;
+            }
+            Console.WriteLine($"basicfare: {BasicFare}");
+        
+        }
+    }
+
+}
---
+++ Oops Concepts/AbstractMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using AbstractMethods;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Flight fli = new Flight();
+        //object initializer - at the time of object creation,initialzie the values
+
+        //abstract class connot be instantiated
+        //it is not mandatory to have abstract method in aabstract
+        //generally we go for abstract  in order to show essential features and hide method implementation
+
+        FlightFare flightFare = new FlightFare()
+        {
+            FlightNo = 11,
+            FlightName = "Airindia",
+            durationinhrs = 6,
+            typedes = "International"
+        };
+
+        flightFare.FlightDetails();
+        flightFare.FareDetails();
+
+        Flight ff = new FlightFare() { FlightNo = 12, FlightName = "Spicejet", durationinhrs = 3, typedes = "Domestic" };
+        ff.FlightDetails();
+        ff.FareDetails();
+    }
+}
