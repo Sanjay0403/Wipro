0ec67a8df0b3aef728e2c88d1a2c172ab512874b
Sanjay0403 <Sanjaydeep098@gmail.com>
2025-01-30 11:05:13 +0530

    Inheritance

---
+++ Oops/Department.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops
+{
+    internal class Department : Employee //inheritance - department
+    {
+        public string? DeptName {  get; set; }
+        public decimal Salary { get; set; }
+
+        //public void Show()
+        //{
+        //    Console.WriteLine(DeptName+ " " +Salary);
+        //}
+
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine(DeptName + " " + Salary);
+
+        }
+    }
+}
---
+++ Oops/Manager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops
+{
+    internal class Manager : Department
+    {
+        public string? ManagerName {  get; set; }
+
+        public override void Display()
+        {
+            base.Display(); //call display method of department class
+            Console.WriteLine("Manager Name: " +ManagerName);
+
+
+        }
+
+    }
+}
