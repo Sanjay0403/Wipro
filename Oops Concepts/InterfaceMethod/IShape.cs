dc60cf686de169fab6ab08fa6cd7d1dc8c022d27
sanjay <118151384+Sanjay0403@users.noreply.github.com>
2025-01-30 15:22:22 +0530

    Interface Method

---
+++ Oops Concepts/InterfaceMethod/IShape.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceMethod
+{
+    //By default, interface is publlic and abstract
+    internal interface IShape
+    {
+        void CalculateArea();
+
+        void DefaultSquare(int side)
+        {
+            Console.WriteLine("area of square: " +Math.Pow(side,2));
+        }
+    }
+
+    //void DefaultSquareArea()
+    //{
+
+    //}
+    class Circle : IShape
+    {
+        public int radius { get; set; }
+        public void CalculateArea()
+        {
+            Console.WriteLine($"Area of circle: {Math.PI * Math.Pow(radius, 2)}");
+        }
+
+    }
+    class Rect : IShape
+    {
+        public int l { get; set; }
+        public int b { get; set; }
+        public void CalculateArea()
+        {
+            Console.WriteLine($"Area of Rectangle: {l*b}");
+        }
+    }
+}
---
+++ Oops Concepts/InterfaceMethod/Program.cs
@@ -0,0 +1,43 @@
+﻿using InterfaceMethod;
+using System.Data.SqlTypes;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string ch;
+        do
+        {
+
+        //Shape shape= new Shape();
+        Console.WriteLine("Enter the shape: 1.Square 2.Circle 3.Rectangle");
+        string? type = Console.ReadLine();
+
+        switch (type)
+        {
+            case "Circle":
+                IShape ish = new Circle() { radius = 10 };
+                ish.CalculateArea();
+                break;
+
+            case "Rectangle":
+                IShape rec = new Rect() { l = 10, b = 5 };
+                rec.CalculateArea();
+                break;
+
+            default:
+                IShape sq = new Circle();
+                sq.DefaultSquare(5);
+                break;
+        }
+
+
+        
+
+        Console.WriteLine("Do you want to continue (yes/no):");
+        ch = Console.ReadLine();
+    } 
+    while (ch.Equals("yes"));
+          
+    }
+}
