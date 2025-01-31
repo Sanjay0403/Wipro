0a073dc3a2e33f4569bb1dfc4ced6495dd1457e0
sanjay <118151384+Sanjay0403@users.noreply.github.com>
2025-01-31 16:14:57 +0530

    Generic Collections

---
+++ Handling Methods/GenericCollections/Sortedlist.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class Sortedlist<T1, T2>
+{
+    internal void Add(int v1, string v2)
+    {
+        throw new NotImplementedException();
+    }
+}
