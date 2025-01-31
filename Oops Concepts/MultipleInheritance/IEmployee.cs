4cfa47f517affca5a1d2c14b7b7d0691adf12548
sanjay <118151384+Sanjay0403@users.noreply.github.com>
2025-01-31 16:10:43 +0530

    Multiple Inheritance

---
+++ Oops Concepts/MultipleInheritance/IEmployee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleInheritance
+{
+    internal interface IEmployee
+    {
+        void BasicDetails();
+
+        void SalaryDetails();
+    }
+        interface IMeeting
+    {
+        void AttendMeetings();
+
+    }
+
+    class PermanentEmployee : IEmployee, IMeeting //multiple inheritance
+    {
+            public int empid { get; set; }
+            public string? empname { get; set; }
+            public decimal salary { get; set; }
+
+            public void BasicDetails()
+            {
+                Console.WriteLine(empid + " " + empname);
+            }
+
+            public void SalaryDetails()
+            {
+                Console.WriteLine(salary);
+            }
+        }
+        class TemporaryEmployee : IEmployee
+        {
+            public int empid { get; set; }
+            public string? empname { get; set; }
+            public decimal salary { get; set; }
+
+            public void AttendMeeting
+        {
+
+        }
+
+            public void BasicDetails()
+            {
+                Console.WriteLine(empid + " " + empname);
+            }
+
+            public void SalaryDetails()
+            {
+                Console.WriteLine(salary);
+            }
+        }
+    }
+
