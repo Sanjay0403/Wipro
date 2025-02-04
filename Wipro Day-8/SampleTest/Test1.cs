00e60d3099868dbdc8b54a82d4b5e999adcfd166
sanjay <118151384+Sanjay0403@users.noreply.github.com>
2025-02-04 15:43:21 +0530

    Testing - MS unit, N unit, X unit

---
+++ Wipro Day-8/SampleTest/Test1.cs
@@ -0,0 +1,41 @@
+﻿using SampleTest;
+
+namespace TestMathOp
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            MathOp mth = new MathOp();
+            int res = mth.Add(20, 20);
+            Assert.AreEqual(res,40);
+        }
+
+        [TestMethod]
+        public void TestMethodSub()
+        {
+            MathOp mth = new MathOp();
+            int res = mth.Sub(50,20);
+            Assert.AreEqual(res, 30);
+            //int res2 = mth.Sub(60, 20);
+            //Assert.AreEqual(res2, 40);
+        }
+        [TestMethod]
+        public void TestMethodSub2()
+        {
+            MathOp mth = new MathOp();
+            int res2 = mth.Sub(60, 20);
+            Assert.AreEqual(res2, 50); //failed test case
+        }
+        [TestMethod]
+        public void TestMethodProd()
+        {
+            MathOp mth = new MathOp();
+            int res = mth.Mul(50, 40);
+            Assert.AreEqual(res, 2000);
+        }
+
+    }
+}
