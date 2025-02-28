daaba954a5c97ccf601f296bfcbe5abb2648ae2c
sanjay <118151384+Sanjay0403@users.noreply.github.com>
2025-02-28 09:58:56 +0530

    Add files via upload

---
+++ WIPRO DAY-21/web-api-demo-master/DemoWebApi/Controllers/StudentController.cs
@@ -0,0 +1,101 @@
+﻿using DemoWebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        static List<Student> students = null;
+        public StudentController()
+        {
+            if (students == null)
+            {
+                students = new List<Student>()
+                {
+                    new Student()
+                    {
+                        Id = 1,
+                        Name = "ajay",
+                        Batch = "B001",
+                        JoiningDate = new DateTime(),
+                        Marks = 90
+                    },
+
+                    new Student()
+                    {
+                        Id = 2,
+                        Name = "vijay",
+                        Batch = "B002",
+                        JoiningDate = new DateTime(),
+                        Marks = 91
+                    },
+
+                    new Student()
+                    {
+                        Id = 3,
+                        Name = "jay",
+                        Batch = "B001",
+                        JoiningDate = DateTime.Parse("12/12/2024"),
+                        Marks = 90
+                    },
+
+                    new Student()
+                    {
+                        Id = 4,
+                        Name = "deepak",
+                        Batch = "B001",
+                        JoiningDate = new DateTime(),
+                        Marks = 90
+                    },
+
+                    new Student()
+                    {
+                        Id = 5,
+                        Name = "gagan",
+                        Batch = "B001",
+                        JoiningDate = new DateTime(),
+                        Marks = 90
+                    }
+                };
+            }
+        }
+
+        // crud , r
+        [HttpGet]
+        public List<Student> Get()
+        {
+            return students;
+
+        }
+        [HttpGet("{id}")]  // read
+        public Student GetById(int id)
+        {
+            return students.FirstOrDefault(x => x.Id == id);
+
+        }
+        [HttpPost]  // create
+
+        public void AddStudent(Student student)
+        {
+            students.Add(student);
+        }
+        [HttpDelete("{id}")]
+        public void DeleteStudent(int id)
+        {
+            Student student = students.FirstOrDefault(x => x.Id == id);
+            students.Remove(student);
+        }
+        [HttpPut("{id}")]
+        public void EditStudent(int id, Student student)
+        {
+            Student obj = students.FirstOrDefault(x => x.Id == id);
+            obj.Batch = student.Batch;
+        }
+
+    }
+
+    }
+
---
+++ WIPRO DAY-21/web-api-demo-master/DemoWebApi/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace DemoWebApi.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Batch { get; set; }
+        public DateTime JoiningDate { get; set; }
+        public int Marks { get; set; }
+    }
+}
+
+// CRUD operations on Student ent Entity
+// Web Api > HTTP Service (It uses HTTP/HTTPS protocol)
+// It uses HTTP Verbs to perform operations
+// Get    > Getting data from web api
+// Post   > Used to post / send data (Create)
+// Put    > Edit data
+// Delete > Delete Data
