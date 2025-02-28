8b4cfce1d71c26e6343bc2b92346bd308363e296
sanjay <118151384+Sanjay0403@users.noreply.github.com>
2025-02-28 09:55:55 +0530

    WIPRO DAY-20

---
+++ WIPRO DAY-20/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentApi.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Batch { get; set; }
+        public DateTime JoiningDate { get; set; }
+        public int Marks { get; set; }
+    }
+}
